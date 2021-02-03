8330f2fc0c98a595ed1c3401452e3356def7f3ae
Alan <alan.wilmanski@gmail.com>
2021-02-04 00:12:43 +0100

    initial commit with files from start.spring.io

---
+++ src/main/java/com/wilmanski/mealassembler/MealAssemblerApplication.java
@@ -0,0 +1,13 @@
+package com.wilmanski.mealassembler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealAssemblerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealAssemblerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wilmanski/mealassembler/MealAssemblerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wilmanski.mealassembler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealAssemblerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
